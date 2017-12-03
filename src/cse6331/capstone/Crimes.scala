57682f08eb0aaa4384e26cc72ab27261c0979111
shahsk0901 <shah.sk0901@gmail.com>
2017-12-02 20:43:17 -0600

    k means clustering using mllib

--- src/cse6331/capstone/Crimes.scala
+++ src/cse6331/capstone/Crimes.scala
@@ -2,28 +2,46 @@ package cse6331.capstone
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.mllib.clustering._
+import org.apache.spark.mllib.linalg.Vectors
 
 object Crimes {
 	def main(args: Array[String]) {
-		val conf = new SparkConf().setAppName("Crimes").setMaster("local[2]")
-		val sc = new SparkContext(conf)
-		var i = 0
 		
+	  // Defining spark context and configuration
+	  val conf = new SparkConf().setAppName("Crimes").setMaster("local[2]")
+		val sc = new SparkContext(conf)
+		
+		// Reading Input and filtering out any rows that has a null value
 		var input = sc.textFile(args(0)).map(line=>{
 		  var x = line.split(",") 
 		  if(x.length == 4) {
-		    (x(0).toString(),x(1).toString(),x(2).toInt,x(3).toInt)
+		    var time = x(0).toString().split(" ")
+		    ((time(1) + "" + time(2)),x(1).toString(),x(2).toInt,x(3).toInt)
 		  } else {
-		    (null,null,null,null)
+		    (null,null,null.asInstanceOf[Int],null.asInstanceOf[Int])
 		  }
+		}).filter(x => x._1 != null)
+		
+		
+		var transformInput = input.map(input => {
+		  var time = input._1.map(_.toByte.doubleValue()).reduceLeft((x,y) => x+y)
+		  var crimeType = input._2.map(_.toByte.doubleValue()).reduceLeft((x,y) => x+y)
+		  (time,crimeType,input._3,input._4)
 		})
 		
-		input = input.filter(x => x._1 != null)
 		
+		val vectors = transformInput.map(input => Vectors.dense(input._1,input._2,input._3.toDouble,input._4.toDouble))
 		
-		println("Count: " + input.count())
+		val clusters = 10
+		val iterations = 20
+		var p = KMeans.train(vectors, clusters, iterations)
+		
+		p.clusterCenters.foreach(println)
+	
+		var prediction = p.predict(vectors)
+		prediction.foreach(println)
+		
 		sc.stop()
-
 	}
 }
