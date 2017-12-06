138d9030b0aa412bdb16891f4f710c33bddf6114
shahsk0901 <shah.sk0901@gmail.com>
2017-12-06 17:18:37 -0600

    function for calculating distance and finding closest cluster implemented

--- src/cse6331/capstone/Crimes.scala
+++ src/cse6331/capstone/Crimes.scala
@@ -2,12 +2,45 @@ package cse6331.capstone
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
-import org.apache.spark.mllib.clustering._
+import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.mllib.linalg.Vectors
 
 object Crimes {
+  
 	def main(args: Array[String]) {
 		
+	  // Calculating squared distance between a record and a cluster centroid
+	  def calculateDistance(record: (Double,Double,Double,Double),centroid: ((Double,Double,Double,Double))) = {
+	    (Math.pow(record._1 - centroid._1, 2) + 
+	    Math.pow(record._2 - centroid._2, 2) + 
+	    Math.pow(record._3 - centroid._3, 2) +
+	    Math.pow(record._4 - centroid._4, 2))
+	  }
+	  
+	  def add(record1: (Double,Double,Double,Double),record2: ((Double,Double,Double,Double))) = {
+	    (record1._1 + record2._1,
+	    record1._2 + record2._2,
+	    record1._3 + record2._3,
+	    record1._4 + record2._4)
+	  }
+	  
+	  // Finding the cluster centroid to which the record is closest to and returning the cluster id
+	  def closestToCluster(record: (Double, Double, Double, Double), clusters: Array[(Double, Double, Double, Double)]): Int = {
+			  
+	    var clusterAssigned = 0	  
+			var minimumDistance = Double.PositiveInfinity
+
+		  for (i <- 1 to clusters.length) {
+			  val distanceToCentroid = calculateDistance(record, clusters(i-1))
+					  if (distanceToCentroid < minimumDistance) {
+						  minimumDistance = distanceToCentroid
+								  clusterAssigned = i
+					  }
+		  }
+      clusterAssigned
+	  }
+	  
+	  
 	  // Defining spark context and configuration
 	  val conf = new SparkConf().setAppName("Crimes").setMaster("local[2]")
 		val sc = new SparkContext(conf)
@@ -27,20 +60,42 @@ object Crimes {
 		var transformInput = input.map(input => {
 		  var time = input._1.map(_.toByte.doubleValue()).reduceLeft((x,y) => x+y)
 		  var crimeType = input._2.map(_.toByte.doubleValue()).reduceLeft((x,y) => x+y)
-		  (time,crimeType,input._3,input._4)
+		  var district = input._3.toDouble
+		  var community = input._4.toDouble
+		  (time,crimeType,district,community)
 		})
 		
+	  // Initializing the RDD to assign the cluster to each point
+		var closestTo = transformInput.map(x => (0,(x,0)))
 		
-		val vectors = transformInput.map(input => Vectors.dense(input._1,input._2,input._3.toDouble,input._4.toDouble))
+		// Randomly initialize cluster centroids
+		var clusters = transformInput.takeSample(false,10,System.nanoTime())
 		
-		val clusters = 10
-		val iterations = 20
-		var p = KMeans.train(vectors, clusters, iterations)
 		
-		p.clusterCenters.foreach(println)
-	
-		var prediction = p.predict(vectors)
-		prediction.foreach(println)
+		var iterations = 20
+	  var currentIteration = 1
+	 
+		while (currentIteration <= iterations ) {
+		  
+		  closestTo = transformInput.map(record => { 
+		    var clusterAssigned = closestToCluster(record, clusters)
+		    (clusterAssigned, (record,1))
+		    
+		  })
+		 
+		  val calculateClusters = closestTo.reduceByKey {
+		    case ((record1,r1), (record2,r2)) => (add(record1,record2),r1+r2) 
+		  }
+		  
+		  
+		  
+		  currentIteration+=1
+		}
+	  
+	  closestTo.foreach(println)
+		
+		//val vectors = transformInput.map(input => Vectors.dense(input._1,input._2,input._3,input._4))
+	  
 		
 		sc.stop()
 	}
